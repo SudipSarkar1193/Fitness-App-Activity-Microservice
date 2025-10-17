0270acdf1bb9bc15a52bc605dda9684c7d97b51b
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2025-10-17 20:18:59 +0530

    Added Activity Model

---
+++ src/main/java/com/fitness/activity_service/models/Activity.java
@@ -0,0 +1,41 @@
+package com.fitness.activity_service.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
+@Document(collection = "activities")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Activity {
+    private long id;
+    private UUID activityUuid;
+    private UUID userUuid;
+    private String name;
+    private ActivityType type;
+    private long duration; // in seconds
+    private long caloriesBurned;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    @Field("additional_data")
+    private Map<String, Object> additionalData;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
---
+++ src/main/java/com/fitness/activity_service/models/ActivityType.java
@@ -0,0 +1,26 @@
+package com.fitness.activity_service.models;
+
+public enum ActivityType {
+    RUNNING,
+    CYCLING,
+    SWIMMING,
+    WALKING,
+    YOGA,
+    STRENGTH_TRAINING,
+    DANCE,
+    PILATES,
+    CARDIO,
+    RESISTANCE_TRAINING,
+    MEDITATION,
+    SPORTS,
+    MARTIAL_ARTS,
+    JUMP_ROPE,
+    JUDO,
+    BOXING,
+    SKIING,
+    SNOWBOARDING,
+    CLIMBING,
+    MOUNTAIN_BIKING,
+    BIKING,
+    OTHER
+}
